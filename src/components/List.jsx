d265625eac19b60cff74887fd1657d7c69cf31b1
Milena Oswald <milenaoswald@Milenas-Air.fritz.box>
2024-02-20 19:44:19 +0100

    List CSS styling

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -7,10 +7,19 @@ const List = () => {
                 <h2>Wer?</h2>
                 <h2>Wann?</h2>
             </div>
-            <div class="toDoList">
-                <span>Test</span>
-                <span>Test</span>
-                <span>Test</span>
+            <div class="list">
+                <label>
+                    <input type="checkbox" />
+                </label>
+                <ul class="ulToDoList">
+                    <li>Test Einkaufen lAlalalla</li>
+                    <li>PterMaria Hubrt</li>
+                    <li>Morgen</li>
+                </ul>
+                <span class="ul-btn">
+                    <button>Ändern</button>
+                    <button>Löschen</button>
+                </span>
             </div>
         </> 
         )
